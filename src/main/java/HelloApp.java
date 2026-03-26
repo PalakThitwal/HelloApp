a49ee9ff7c41958a8a96736f5a85b3bb78e84c0e
PalakThitwal <palakthitwal@gmail.com>
2026-03-26 08:24:30 +0530

    Coded tot print Hello World

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 public class HelloApp {
     public static void main(String[] args){
-        String name ="World";
-        if(args.length>0) {
-            name = args[0];
-        }
+        String name = args[0];
         System.out.println("Hello," + name + "!");
     }
 }
